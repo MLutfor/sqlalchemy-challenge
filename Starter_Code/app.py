307926721b0e852ddfd640cef73a36fb5e2c1405
MLutfor <142198802+MLutfor@users.noreply.github.com>
2023-10-26 18:31:48 -0400

    f

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,5 +1,5 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
 
 
 #################################################
@@ -21,10 +21,22 @@
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Justice League API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/justice-league"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
